9ceec6a50e12b3bc737e452a967c6ab530eefc4b
Alexander Sabayle <alexander.sabayle@gmail.com>
2022-01-28 09:02:07 -0500

    changed every window to be a standard 1440 x 900 pixels

--- Battleship-2.0/Battleship-2.0/LeaderBoard.Designer.cs
+++ Battleship-2.0/Battleship-2.0/LeaderBoard.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // LeaderBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1424, 861);
+            this.Name = "LeaderBoard";
             this.Text = "LeaderBoard";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
