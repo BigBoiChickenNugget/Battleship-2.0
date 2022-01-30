075172306f3397a1ffb8dc5b127d6dc0e4885ab2
Talha Ahmad <talha05ahmad@gmail.com>
2022-01-30 09:35:56 -0500

    Ship AI can now choose its own positions.

--- Battleship-2.0/Battleship-2.0/AI.cs
+++ Battleship-2.0/Battleship-2.0/AI.cs
@@ -41,46 +41,101 @@ namespace Battleship_2._0
             // Create a random object.
             Random randomGenerator = new Random();
 
-            // Get positions for carrier ship.
-            // Randomly get the orientation of the ship. If it's 1, it's horizontal; if it's 2, it's vertical.
-            //int orientation = randomGenerator.Next(1, 3);
-            int orientation = 1;
+            // The initial size of the ship is 5, because the first ship done will be the carrier.
+            int size = 5;
 
-            // If the ship is horizontal.
-            if (orientation == 1)
+            // Variable to see if the submarine has been placed yet.
+            bool submarinePlaced = false;
+
+            while (size > 1)
             {
-                // Randomly generate the row and the column that the left-most cell of the ship should be on.
-                int row = randomGenerator.Next(1, 11);
-                int column = randomGenerator.Next(1, 7);
-                int startingCell = column + (10 * (row - 1));
-
-                for (int index = startingCell; index < startingCell+5; index++)
+                // Get positions for the battleship ship. Must be in a while loop because it's possible that the position it's placed on is already taken.
+                while (true)
                 {
-                    ships[index - 1] = true;
+                    // Generate the orientation of the ship.
+                    int orientation = randomGenerator.Next(1, 3);
+
+                    // If the ship is horizontal.
+                    if (orientation == 1)
+                    {
+                        // Randomly generate the row and the column that the left-most cell will be on.
+                        int row = randomGenerator.Next(1, 11);
+                        int column = randomGenerator.Next(1, 11-(size-1));
+                        int startingCell = column + (10 * (row - 1));
+
+                        // Variable to see if the position played is valid (cells aren't taken by another cell).
+                        bool validPosition = true;
+
+                        // Check to see if another ship lies in the cells.
+                        for (int index = startingCell; index < startingCell+size; index++)
+                        {
+                            if (ships[index - 1] == true)
+                            {
+                                validPosition = false;
+                                break;
+                            }
+                        }
+
+                        // If the positions are taken, start the while loop again.
+                        if (validPosition == false)
+                            continue;
+
+                        // Set the cells the ship lies on to true, signifying that there're ships in those cells.
+                        for (int index = startingCell; index < startingCell+size; index++)
+                        {
+                            ships[index - 1] = true;
+                        }
+
+                        // Break while loop.
+                        break;
+                    }
+
+                    // Run this code if the ship is meant to be placed vertically.
+                    else if (orientation == 2)
+                    {
+                        // Randomly generate the row and the column that the top-most cell will be on.
+                        int row = randomGenerator.Next(1, 11-(size-1));
+                        int column = randomGenerator.Next(1, 11);
+                        int startingCell = column + (10 * (row - 1));
+
+                        // Variable to see if the position the ship will be on are already taken by another ship.
+                        bool validPosition = true;
+
+                        // Check to see if the positions are taken.
+                        for (int index = startingCell; index < startingCell + (10 * (size - 1)) + 1; index++)
+                        {
+                            // If the spot is taken, set validPosition to false, signifying that this spot is taken and break the for loop.
+                            if (ships[index - 1] == true)
+                            {
+                                validPosition = false;
+                                break;
+                            }
+                        }
+
+                        // If validPosition is false, start the while loop again.
+                        if (validPosition == false)
+                            continue;
+                        
+                        // If validPosition is true, set the cells to true.
+                        for (int index = startingCell; index < startingCell + (10 * (size - 1)) + 1; index += 10)
+                        {
+                            // Set the cell to true.
+                            ships[index - 1] = true;
+                        }
+
+                        // Break the while loop.
+                        break;
+                    }
                 }
-            }
 
-            else if (orientation == 2)
-            {
-                // Randomly generate the now and the column of the top-most cell the ship should be on.
-                int row = randomGenerator.Next(1, 7);
-                int column = randomGenerator.Next(1, 7);
-
-                for (int index = (column + (10 * (row-1))); index < index + (10 * row) + 1; index++)
+                // Deincrement the size variable signifying that the ship have been successfully placed; however, if the size variable is 3, then keep it at 3 because there are two ships at size 3.
+                if (size == 3 && submarinePlaced == false)
                 {
-                    
-                    ships[index - 1] = true;
+                    submarinePlaced = true;
+                    continue;
                 }
-            }
 
-            // Get positions for the cruiser ship. Must be in a while loop because it's possible that the position it's placed on is already taken.
-            while (true)
-            {
-                // Generate the orientation of the ship.
-                orientation = randomGenerator.Next(1, 3);
-
-                // If the ship is horizontal.
-                break;
+                size--;
             }
         }
     }
