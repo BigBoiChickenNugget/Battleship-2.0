d1bd07905a8b32d8f54ee1797288dba7c5e27ff3
Alexander Sabayle <alexander.sabayle@gmail.com>
2022-01-31 21:03:01 -0500

    revamped game screen (not yet done) and reduced game lag

--- Battleship-2.0/Battleship-2.0/Properties/Resources.Designer.cs
+++ Battleship-2.0/Battleship-2.0/Properties/Resources.Designer.cs
@@ -283,6 +283,16 @@ namespace Battleship_2._0.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap oceantiles {
+            get {
+                object obj = ResourceManager.GetObject("oceantiles", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PlayerEmpty {
             get {
                 object obj = ResourceManager.GetObject("PlayerEmpty", resourceCulture);
